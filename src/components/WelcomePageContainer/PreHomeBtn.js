b0de6e4158ded33d2a5ab710a0e876dcce05a3bd
minamalaj <mina.malaj@hotmail.com>
2020-09-30 14:08:18 +0200

    Pre-Home CSS and button

---
+++ src/components/WelcomePageContainer/PreHomeBtn.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+const PreHomeBtn = () => { 
+    return ( 
+        <>
+            <div class="box-3">
+                <div class="btn btn-three">
+                    <span>Browse pitches near you</span>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default PreHomeBtn; 
