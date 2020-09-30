ffe1fbb2781205e92450773c9e55adb45951c7d1
minamalaj <mina.malaj@hotmail.com>
2020-09-30 14:28:49 +0200

    App file and other minor adjustments to code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PreHome from './components/WelcomePageContainer/PreHome'
 import AboutPage from './components/AboutPageContainer/AboutPage'
 import PitchProposalForm from "./components/PitchProposalContainer/PitchProposalForm";
+import Homepage from "./MapContainer/Homepage";
+import AdminDashboardContainer from "./components/AdminPageContainer/AdminDashboardContainer";
+import Error from "./components/ErrorPageContainer/Error";
+import "./App.css";
 
 class App extends React.Component {
   render() {
@@ -11,8 +15,11 @@ class App extends React.Component {
         <Router>
           <Switch>
               <Route exact path="/" component={PreHome} />
+              <Route path="/find-pitch" component={Homepage} />
               <Route path="/about" component={AboutPage} />
               <Route path="/suggest-site" component={PitchProposalForm} />
+              <Route path="/admin" component={AdminDashboardContainer} />
+              <Route path="*" component={Error} />
           </Switch>
         </Router>
       </>
